73c79cc1ce639c4b3391dc34d51de17127b45fdf
Milkitic <Milkitic@126.com>
2022-07-22 13:27:38 +0800

    Update libs

--- src/OsbOptimizer/ViewModels/OptimizeViewModel.cs
+++ src/OsbOptimizer/ViewModels/OptimizeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Coosu.Storyboard;
 using Coosu.Storyboard.Extensions.Optimizing;
@@ -130,7 +131,7 @@ namespace Milki.OsbOptimizer.ViewModels
                     SituationOutput = new ObservableCollection<string>();
                     ErrorOutput = new ObservableCollection<string>();
                     _group = await Layer.ParseFromFileAsync(FilePath);
-                    _compressor = new SpriteCompressor(_group, new CompressSettings
+                    _compressor = new SpriteCompressor(_group, new CompressOptions()
                     {
                         ThreadCount = 2
                     });
@@ -170,7 +171,7 @@ namespace Milki.OsbOptimizer.ViewModels
                         //    Thread.Sleep(1);
                         //}
                     }
-                    void OnCompressorOnSituationFound(object sender, SituationEventArgs e)
+                    async Task OnCompressorOnSituationFound(object sender, SituationEventArgs e)
                     {
                         if (e.Sprite == null)
                         {
